397f909df281e4db927bac2809aa2b75e0f21939
kunkka0822 <hungryAdmiral0512@gmail.com>
2024-05-09 23:41:36 +0200

    feat: modify webhook workflow

--- src/pages/api/docusign/index.ts
+++ src/pages/api/docusign/index.ts
@@ -1,22 +1,29 @@
+import { getCookie } from '@/lib/utils'
+import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === 'POST') {
-    // Receive the JSON payload
     const payload = req.body
-    console.log('====================================')
-    console.log(payload)
-    if (
-      payload.envelopeStatus &&
-      payload.envelopeStatus.status === 'completed'
-    ) {
-      console.log('Envelope has been completed')
-      // Process the event (e.g., update database or send email)
+    if (payload && payload.event) {
+      const session = getCookie(req)
+      const response = await axios.post(
+        process.env.NEXT_PUBLIC_BASE_URL + '/contract/notify',
+        { data: payload },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: session,
+          },
+        }
+      )
     }
 
     res.status(200).json({ message: 'Webhook received and processed' })
   } else {
-    // Not a POST request
     res.setHeader('Allow', ['POST'])
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
