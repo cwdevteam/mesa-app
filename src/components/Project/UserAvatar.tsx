bdf9023afeccebfaeb28217416b13f30381fd3f6
kunkka0822 <hungryAdmiral0512@gmail.com>
2024-05-31 08:51:33 +0200

    chore: resolve user avatar issue

--- src/components/Project/UserAvatar.tsx
+++ src/components/Project/UserAvatar.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react'
-
 export type UserAvatarProps = {
-  user: UserProps
+  user?: UserProps
 }
 
 const UserAvatar: React.FC<UserAvatarProps> = ({ user }) => {
@@ -9,10 +7,14 @@ const UserAvatar: React.FC<UserAvatarProps> = ({ user }) => {
     <button className="inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-12 w-12 rounded-full">
       <span className="relative flex shrink-0 overflow-hidden rounded-full h-11 w-11">
         <span className="flex h-full w-full items-center justify-center rounded-full bg-muted">
-          {!user.avatar ? (
-            user.nick_name[0] + user.nick_name[1]
+          {user ? (
+            !user.avatar ? (
+              user.nick_name[0] + user.nick_name[1]
+            ) : (
+              <img src={user.avatar} alt="avatar" />
+            )
           ) : (
-            <img src={user.avatar} alt="avatar" />
+            <></>
           )}
         </span>
       </span>
